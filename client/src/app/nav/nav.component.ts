9a8bf533dc09c6f9373c6d171f409ad5911e1fe5
devthet <devthet@hotmail.com>
2021-11-07 14:37:31 +0630

    Routing and Theme

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -13,7 +15,7 @@ export class NavComponent implements OnInit {
   model:any = {}
   //currentUser$:Observable<User>
  // loggedIn:boolean
-  constructor(public account:AccountService) { }
+  constructor(public account:AccountService,private router:Router,private toastr:ToastrService) { }
 
   ngOnInit(): void {
     //this.getCurrentUser()
@@ -22,17 +24,21 @@ export class NavComponent implements OnInit {
   login(){
 
     this.account.login(this.model).subscribe(response=>{
-      console.log(response)
+      this.router.navigateByUrl('/members')
+      //console.log(response)
     //  this.loggedIn = true
     },
     err=>{
       console.error(err)
+      this.toastr.error(err.error)
+      
     }
 
     )
   }
   logout(){
     this.account.logout()
+    this.router.navigateByUrl('/')
    // this.loggedIn = false
 
   }
